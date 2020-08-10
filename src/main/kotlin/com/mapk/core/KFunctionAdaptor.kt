403830bd031ca11cfb1e0128587ecb284a35d566
wrongwrong <boranti1995@gmail.com>
2020-08-10 17:50:27 +0900

    パラメータ類の変更を取り入れput周りを修正

--- src/main/kotlin/com/mapk/core/KFunctionAdaptor.kt
+++ src/main/kotlin/com/mapk/core/KFunctionAdaptor.kt
@@ -7,44 +7,52 @@ class KFunctionAdaptor<T> internal constructor(
     private val function: KFunction<T>,
     private val index: Int?,
     myParameters: List<ValueParameter<*>>,
-    private val bucket: ArgumentBucket,
+    private val myBucket: ArgumentBucket,
     private val children: List<KFunctionAdaptor<*>>
 ) {
+    private var count = myBucket.initialCount
     private val requiredParameterMap: Map<String, Pair<ValueParameter<*>, ArgumentBucket>>
 
     init {
-        val thisMap = myParameters.associate { it.name to (it to bucket) }
+        val thisMap = myParameters.associate { it.name to (it to myBucket) }
 
         requiredParameterMap = children
             .map { it.requiredParameterMap }
             .fold(thisMap) { acc, cur -> acc + cur }
     }
 
-    val isFullInitialized: Boolean get() = bucket.isFullInitialized
+    // TODO: ここでいう初期化状態は子まで全て含んだものになるため修正する
+    val isFullInitialized: Boolean get() = myBucket.valueArray.size == count
 
-    // TODO: forcePutとか考えると、初期化状況の管理はAdaptorに持たせるのが妥当という気がするので考える
     fun putIfAbsent(key: String, value: Any?) {
-        requiredParameterMap.getValue(key).let {
-            // TODO: bucketのインターフェースを実情に合わせて検討する
-            if (!it.bucket.containsKey(it.parameter)) it.bucket.putIfAbsent(it.parameter.index, value)
+        requiredParameterMap.getValue(key).let { (param, bucket) ->
+            if (!bucket.initializationStatuses[param.index]) {
+                bucket.forcePut(param.index, value)
+                count++
+            }
         }
     }
 
     fun putIfAbsent(key: String, consumer: () -> Any?) {
-        requiredParameterMap.getValue(key).let {
-            if (!it.bucket.containsKey(it.parameter)) it.bucket.putIfAbsent(it.parameter.index, consumer())
+        requiredParameterMap.getValue(key).let { (param, bucket) ->
+            if (!bucket.initializationStatuses[param.index]) {
+                bucket.forcePut(param.index, consumer())
+                count++
+            }
         }
     }
+
+    // TODO: forcePut関数を作る
 
     fun call(): T {
         children.forEach {
             // 子ならindexは入ってる想定
-            bucket.putIfAbsent(it.index!!, it.call())
+            myBucket.forcePut(it.index!!, it.call())
         }
 
-        return if (bucket.isFullInitialized)
-            function.call(*bucket.valueArray)
+        return if (isFullInitialized)
+            function.call(*myBucket.valueArray)
         else
-            function.call(bucket)
+            function.call(myBucket)
     }
 }
