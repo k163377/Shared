59ad59601e4f8abac5e65e4de1c2cc9f80396338
wrongwrong <boranti1995@gmail.com>
2020-08-10 14:48:16 +0900

    初期化時のチェック処理を追加

--- src/main/kotlin/com/mapk/core/KFunctionAdaptorGenerator.kt
+++ src/main/kotlin/com/mapk/core/KFunctionAdaptorGenerator.kt
@@ -3,6 +3,7 @@ package com.mapk.core
 import com.mapk.core.internal.BucketGenerator
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
+import kotlin.reflect.jvm.isAccessible
 
 class KFunctionAdaptorGenerator<T>(
     private val function: KFunction<T>,
@@ -14,6 +15,14 @@ class KFunctionAdaptorGenerator<T>(
     private val valueParameters: List<KParameter> = TODO()
     private val childGenerators: List<KFunctionAdaptorGenerator<*>> = TODO()
     private val bucketGenerator: BucketGenerator = BucketGenerator(parameters, instance)
+
+    init {
+        if (parameters.isEmpty() || (instance != null && parameters.size == 1))
+            throw IllegalArgumentException("This function is not require arguments.")
+
+        // この関数には確実にアクセスするためアクセシビリティ書き換え
+        function.isAccessible = true
+    }
 
     fun generateAdaptor(): KFunctionAdaptor<T> {
         return KFunctionAdaptor(
