2a8ebe1c28974b5e27f57dbb7bc5bd6b11399adf
wrongwrong <boranti1995@gmail.com>
2020-08-15 15:13:29 +0900

    バケツジェネレータ周りを修正

--- src/main/kotlin/com/mapk/core/internal/BucketGenerator.kt
+++ src/main/kotlin/com/mapk/core/internal/BucketGenerator.kt
@@ -2,32 +2,35 @@ package com.mapk.core.internal
 
 import kotlin.reflect.KParameter
 
-internal class BucketGenerator(private val keyList: List<KParameter>, instance: Any?) {
-    private val originalInitializationStatus: Array<Boolean>
+internal class BucketGenerator private constructor(
+    private val keyList: List<KParameter>,
+    private val originalInitializationStatus: Array<Boolean>,
     private val originalValueArray: Array<Any?>
-    private val originalCount: Int
-
-    init {
-        val capacity = keyList.size
-
-        originalInitializationStatus = Array(capacity) { false }
-        originalValueArray = arrayOfNulls(capacity)
-
-        if (instance != null) {
-            originalValueArray[0] = instance
-            originalInitializationStatus[0] = true
-            originalCount = 1
-        } else {
-            originalCount = 0
-        }
-    }
-
+) {
     fun generate(): ArgumentBucket {
         return ArgumentBucket(
             keyList,
             originalValueArray.clone(),
-            originalInitializationStatus.clone(),
-            originalCount
+            originalInitializationStatus.clone()
         )
+    }
+
+    companion object {
+        fun of(keyList: List<KParameter>, instance: Any?): Pair<BucketGenerator, Int> {
+            val capacity = keyList.size
+
+            val originalInitializationStatus: Array<Boolean> = Array(capacity) { false }
+            val originalValueArray: Array<Any?> = arrayOfNulls(capacity)
+
+            val initialCount: Int = if (instance != null) {
+                originalValueArray[0] = instance
+                originalInitializationStatus[0] = true
+                1
+            } else {
+                0
+            }
+
+            return BucketGenerator(keyList, originalInitializationStatus, originalValueArray) to initialCount
+        }
     }
 }
